50d2d8f93a37e08620c1560860f13213394e1658
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-05 14:53:18 +0530

    flip coin is tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -27,10 +27,10 @@ do
    	then
       	break
    	fi
-
 done
-      echo " Heads wins "$HEAD_CNT" times "
-      echo " Tails wins "$TAIL_CNT" times "
+echo " Heads wins "$HEAD_CNT" times "
+echo " Tails wins "$TAIL_CNT" times "
+
 
 	if [ $HEAD_CNT -gt $TAIL_CNT ]
    then
@@ -40,9 +40,30 @@ done
    elif [ $HEAD_CNT  -lt $TAIL_CNT ]
    then
       TAIL_WINS=$(($TAIL_CNT-$HEAD_CNT))
-      echo "Tails wins Heads by $TAIL_WINS times"
+      echo " Tails wins Heads by $TAIL_WINS times"
 
    else
       echo " Tie "
    fi
 
+
+	if [ $HEAD_CNT  -eq  $TAIL_CNT ]
+   then
+      while [ $valid ]
+      	RANDOM_CHECK=$((RANDOM%2))
+		do
+     		if [ $RANDOM_CHECK -eq $IS_HEAD ]
+         then
+         	HEAD_CNT=$(($HEAD_CNT+1))
+         else
+    			TAIL_CNT=$(($TAIL_CNT+1))
+         fi
+				HEAD_WINS=$(($HEAD_CNT-$TAIL_CNT))
+            TAIL_WINS=$(($TAIL_CNT-$HEAD_CNT))
+
+         if [[ $HEAD_CNT -eq 2 || $TAIL_CNT -eq 2 ]]
+         then
+         	break
+         fi
+		done
+	fi
